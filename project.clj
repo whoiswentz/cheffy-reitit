800d937a53e68f27d7f2b895bc0113c1f70d1334
Vinicios Wentz <vinicios@wentz.io>
2023-09-24 20:12:08 -0300

    chore(initialization): Project Created

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject cheffy "0.1.0-SNAPSHOT"
+  :description "Cheffy REST API"
+  :url "https://api.learnreitit.com" 
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring "1.10.0"]
+                 [integrant "0.8.1"]
+                 [environ "1.2.0"]
+                 [fi.metosin/reitit "0.7.0-alpha6"]
+                 [com.github.seancorfield/next.jdbc "1.3.883"]
+                 [org.postgresql/postgresql "42.6.0"]
+                 [clj-http "3.12.3"]
+                 [net.clojars.kelveden/ring-jwt "2.4.0"]]
+  :main ^:skip-aot cheffy.core
+  :target-path "target/%s"
+  :uberjar-name "cheffy.jar"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:source-paths ["dev/src"]
+                   :resources-paths ["dev/resources"]
+                   :dependencies [[ring/ring-mock "0.4.0"]
+                                  [integrant/repl "0.3.3"]]}})
