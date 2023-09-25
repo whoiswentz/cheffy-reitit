dac42b85751fc7a48db97c8c4aa6a1fe4bd89f29
Vinicios Wentz <vinicios@wentz.io>
2023-09-25 16:03:12 -0300

    feat(jetty): initial configuration for jetty

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject cheffy "0.1.0-SNAPSHOT"
   :description "Cheffy REST API"
-  :url "https://api.learnreitit.com" 
+  :url "https://api.learnreitit.com"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring "1.10.0"]
                  [integrant "0.8.1"]
@@ -10,7 +11,7 @@
                  [org.postgresql/postgresql "42.6.0"]
                  [clj-http "3.12.3"]
                  [net.clojars.kelveden/ring-jwt "2.4.0"]]
-  :main ^:skip-aot cheffy.core
+  :main ^:skip-aot cheffy.server
   :target-path "target/%s"
   :uberjar-name "cheffy.jar"
   :profiles {:uberjar {:aot :all
--- src/cheffy/server.clj
+++ src/cheffy/server.clj
@@ -1,5 +1,16 @@
 (ns cheffy.server
-  (:gen-class))
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty]))
+
+(def app
+  (ring/ring-handler
+   (ring/router [["/" {:get {:handler (fn [req] {:status 200
+                                                 :body "Hello World"})}}]])))
+
+(defn start
+  []
+  (jetty/run-jetty app {:port 3000 :join? false})
+  (println "\n Server running on port 3000"))
 
 (defn -main
   "I don't do a whole lot ... yet."
