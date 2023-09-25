b82b7c1c88b93a96e5f700e6543114237703e217
Vinicios Wentz <vinicios@wentz.io>
2023-09-25 18:03:50 -0300

    feat(integrant): Integrant basic configuration

--- src/cheffy/server.clj
+++ src/cheffy/server.clj
@@ -1,18 +1,38 @@
 (ns cheffy.server
   (:require [reitit.ring :as ring]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [integrant.core :as ig]
+            [environ.core :refer [env]]))
 
-(def app
+(defn app
+  [env]
   (ring/ring-handler
    (ring/router [["/" {:get {:handler (fn [req] {:status 200
                                                  :body "Hello World"})}}]])))
+(defmethod ig/init-key :server/jetty
+  [_ {:keys [handler, port]}]
+  (jetty/run-jetty handler {:port port :join? false})
+  (println (str "\nServer running on port " port)))
 
-(defn start
-  []
-  (jetty/run-jetty app {:port 3000 :join? false})
-  (println "\n Server running on port 3000"))
+(defmethod ig/halt-key! :server/jetty
+  [_ jetty]
+  (.stop jetty))
+
+(defmethod ig/init-key :cheffy/app
+  [_ config]
+  (app config))
+
+(defmethod ig/init-key :db/postgres
+  [_ config]
+  (:jdbc-url config))
+
+(defmethod ig/prep-key :server/jetty
+  [_ config]
+  (merge config {:port (Integer/parseInt (env :port))}))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [config-file]
+  (-> config-file
+      slurp
+      ig/read-string
+      ig/init))
