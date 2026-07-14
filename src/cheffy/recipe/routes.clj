7fcbaa3ac59b3aec173bb4f9ad3c57f9ad8aef05
Vinicios Wentz <wentz.vinicios@gmail.com>
2026-07-14 17:48:21 -0300

    Fix route schema definitions and add recipe tests

--- src/cheffy/recipe/routes.clj
+++ src/cheffy/recipe/routes.clj
@@ -8,28 +8,28 @@
     ["/recipes" {:swagger {:tags ["recipes"]}}
      [""
       {:get {:handler (recipe/list-all-recipes db)
-             :response {200 {:body responses/recipe}}
-             :summary "List all recipes"}}
-      :post {:handler (recipe/create-recipe! db)
-             :parameter {:body {:name string?
-                                :prep-time number?
-                                :img string?}}
-             :response {201 {:body {:recipe-id string?}}}
-             :summary "Create recipe"}]
+             :responses {200 {:body responses/recipe}}
+             :summary "List all recipes"}
+       :post {:handler (recipe/create-recipe! db)
+              :parameters {:body {:name string?
+                                  :prep-time number?
+                                  :img string?}}
+              :responses {201 {:body {:recipe-id string?}}}
+              :summary "Create recipe"}}]
      ["/:recipe-id"
       {:get {:handler (recipe/retrieve-recipe db)
              :parameters {:path {:recipe-id string?}}
-             :response {200 {:body responses/recipe}}
-             :summary "Retrieve recipe"}}
-      :put {:handler   (recipe/update-recipe! db)
-            :parameter {:path {:recipe-id string?}
-                        :body {:name      string?
-                               :prep-time int?
-                               :public    boolean?
-                               :img       string?}}
-            :responses {204 {:body nil?}}
-            :summary   "Update recipe"}
-      :delete {:handler (recipe/delete-recipe! db)
-            :parameters {:path {:recipe-id string?}}
-            :response {204 {:body nil?}}
-            :summary "Delete recipe"}]]))
+             :responses {200 {:body responses/recipe}}
+             :summary "Retrieve recipe"}
+       :put {:handler   (recipe/update-recipe! db)
+             :parameters {:path {:recipe-id string?}
+                          :body {:name      string?
+                                 :prep-time int?
+                                 :public    boolean?
+                                 :img       string?}}
+             :responses {204 {:body nil?}}
+             :summary   "Update recipe"}
+       :delete {:handler (recipe/delete-recipe! db)
+                :parameters {:path {:recipe-id string?}}
+                :responses {204 {:body nil?}}
+                :summary "Delete recipe"}}]]))
